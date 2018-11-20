7fb0ff49587af044e01879c40a537bb9b3fbc626
Made Baruna <made.setia@gmail.com>
2018-11-20 15:57:21 +0700

    Search

--- index.d.ts
+++ index.d.ts
@@ -124,92 +124,11 @@ interface Product {
   price_old: string
   category_id: number
   menu_id: number
-  tax_group_id: null
-  type: string
-  created_at: string
-  updated_at: string
-  images: string[]
-  tax_value: number
-  city_id: number
-  additional: {
-    topping: [
-      {
-        id: number
-        label: string
-        name: string
-        data: string
-      }
-    ]
-  }
-  merchant: Merchant
-  merchant_id: number
-  formatted_price: string
-  formatted_old_price: string
-  product_images: [
-    {
-      id: number
-      image: string
-      product_id: number
-      created_at: string
-      updated_at: string
-    }
-  ]
-  tax_group: string
-  category: {
-    id: number
-    name: string
-    _lft: number
-    _rgt: number
-    parent_id: string
-    suggest_id: string
-    city_id: number
-    merchant_id: number
-    created_at: string
-    updated_at: string
-    has_children: number
-    image_url: string
-  }
-  product_additional: [
-    {
-      id: number
-      product_id: number
-      label: string
-      name: string
-      price: string
-      created_at: string
-      updated_at: string
-    }
-  ]
-}
-
-interface SearchResponse {
-  success: boolean
-  product_found: number
-  merchant_found: number
-  product_data: Product[]
-  merchant_data: Merchant[]
-}
-
-interface SearchContext extends SearchResponse {
-  resto: SearchRestoResponse
-  search: (type: number) => Promise<boolean>
-  searchBySuggestion: (parentId: number) => Promise<boolean>
-  searchRestoDetail: (menuId: number) => Promise<boolean>
-}
-
-interface Food {
-  id: number
-  name: string
-  description: string
-  price: string
-  price_old: string
-  category_id: number
-  menu_id: number
   tax_group_id: number
   type: string
   created_at: string
   updated_at: string
-  images: [string]
+  images: string[]
   tax_value: number
   city_id: number
   additional: [
@@ -233,23 +152,87 @@ interface Food {
       updated_at: string
     }
   ]
-  tax_group: null
-  category: {
-    id: number
-    name: string
-    _lft: number
-    _rgt: number
-    parent_id: number
-    suggest_id: string
-    city_id: number
-    merchant_id: number
-    created_at: string
-    updated_at: string
-    has_children: number
-    image_url: string
-    type: []
-  }
-  product_additional: []
+  tax_group: string
+  category: Category
+  product_additional: [
+    {
+      id: number
+      product_id: number
+      label: string
+      name: string
+      price: string
+      created_at: string
+      updated_at: string
+    }
+  ]
+}
+
+interface SearchResponse {
+  success: boolean
+  product_found: number
+  merchant_found: number
+  product_data: Product[]
+  merchant_data: Merchant[]
+}
+
+interface SearchContext extends SearchResponse {
+  resto: SearchRestoResponse
+  result: SearchResponse
+  search: (type: number) => Promise<boolean>
+  searchBySuggestion: (parentId: number) => Promise<boolean>
+  searchRestoDetail: (menuId: number) => Promise<boolean>
+  searchByName: (name: string) => Promise<boolean>
+}
+
+interface Food {
+  id: number
+  name: string
+  description: string
+  price: string
+  price_old: string
+  category_id: number
+  menu_id: number
+  tax_group_id: number
+  type: string
+  created_at: string
+  updated_at: string
+  images: string[]
+  tax_value: number
+  city_id: number
+  additional: [
+    {
+      id: number
+      label: string
+      name: string
+      data: string
+    }
+  ]
+  merchant: Merchant
+  merchant_id: number
+  formatted_price: string
+  formatted_old_price: string
+  product_images: [
+    {
+      id: number
+      image: string
+      product_id: number
+      created_at: string
+      updated_at: string
+    }
+  ]
+  tax_group: string
+  category: Category
+  product_additional: [
+    {
+      id: number
+      product_id: number
+      label: string
+      name: string
+      price: string
+      created_at: string
+      updated_at: string
+    }
+  ]
 }
 
 interface MenuData {
--- src/screens/main/MainSearch.tsx
+++ src/screens/main/MainSearch.tsx
@@ -1,20 +1,98 @@
 import React from "react"
-import { View, StyleSheet, Image, ImageStyle } from "react-native"
-import { NavigationStackScreenOptions } from "react-navigation"
+import { View, StyleSheet, Image, ImageStyle, FlatList } from "react-native"
+import {
+  NavigationStackScreenOptions,
+  NavigationScreenProp
+} from "react-navigation"
 
 import Text from "../../components/CustomText"
 import HeaderOverlay from "../../components/HeaderOverlay"
 import metrics from "../../config/metrics"
 import SearchBar from "../../components/SearchBar"
 import SearchItem from "../../components/SearchItem"
+import withSearchContext from "../../components/consumers/withSearchContext"
+import { any } from "prop-types"
 
 const LOGO = require("../../../assets/logo-higres.png")
 const OVERLAY = require("../../../assets/overlay_search.png")
 
-export default class MainSearch extends React.Component {
+interface Result {
+  id: number
+  type: string
+  name: string
+  data?: Food
+}
+
+interface Props {
+  navigation: NavigationScreenProp<any, any>
+  search: SearchContext
+}
+
+interface State {
+  keyword: string
+  timeout: number
+  results: Result[]
+}
+
+class MainSearch extends React.Component<Props, State> {
   static navigationOptions: NavigationStackScreenOptions = {
     title: "Search",
     headerTitle: <Image source={LOGO} />
+  }
+
+  state = {
+    keyword: "",
+    timeout: -1,
+    results: [] as Result[]
+  }
+
+  getResult = async () => {
+    if (this.state.keyword === "") return
+
+    await this.props.search.searchByName(this.state.keyword)
+    const results = this.props.search.result
+
+    const merchantResult: Result[] = results.merchant_data.map(item => ({
+      id: item.id,
+      type: "merchant",
+      name: item.name
+    }))
+
+    const foodResult: Result[] = results.product_data.map(item => ({
+      id: item.id,
+      type: "product",
+      name: item.name,
+      data: item as Food
+    }))
+
+    const parsedResults = [...merchantResult, ...foodResult]
+    this.setState({ results: parsedResults })
+  }
+
+  handleChange = async (text: string) => {
+    await this.setState({ keyword: text })
+
+    clearTimeout(this.state.timeout)
+    this.setState({
+      timeout: setTimeout(this.getResult, 1000)
+    })
+  }
+
+  handleClick = (result: Result, type: string) => () => {
+    if (type === "merchant") {
+      this.props.navigation.navigate("RestoDetail", {
+        merchantId: result.id
+      })
+    } else {
+      const food = result.data as Food
+      console.log(food)
+      this.props.navigation.navigate("FoodDetail", {
+        id: result.id,
+        title: result.name,
+        additional: food.additional,
+        picture: food.images[0]
+      })
+    }
   }
 
   render() {
@@ -23,10 +101,21 @@ export default class MainSearch extends React.Component {
         <HeaderOverlay />
         <View style={styles.searchContainer}>
           <Image source={OVERLAY} style={styles.overlay as ImageStyle} />
-          <SearchBar style={{ borderWidth: 0 }} />
-          <SearchItem label={"MCD"} />
-          <SearchItem label={"Dunkin"} />
-          <SearchItem label={"Holycow"} />
+          <SearchBar
+            autoFocus
+            onChangeText={this.handleChange}
+            style={{ borderWidth: 0 }}
+          />
+          <FlatList
+            data={this.state.results}
+            keyExtractor={item => `${item.type}.${item.id}`}
+            renderItem={({ item }) => (
+              <SearchItem
+                label={item.name}
+                onPress={this.handleClick(item, item.type)}
+              />
+            )}
+          />
         </View>
       </View>
     )
@@ -44,7 +133,8 @@ const styles = StyleSheet.create({
     flex: 1,
     borderRadius: 5,
     width: metrics.DEVICE_WIDTH * 0.9,
-    marginTop: 20
+    marginTop: 20,
+    marginBottom: 20
   },
 
   overlay: {
@@ -53,3 +143,5 @@ const styles = StyleSheet.create({
     top: metrics.DEVICE_HEIGHT * 0.3
   }
 })
+
+export default withSearchContext(MainSearch)
