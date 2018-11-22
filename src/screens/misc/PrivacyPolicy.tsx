5453505663d4e25437c562857dd0bebb66e68eca
Ananta Pratama <rinna@Anantas-MacBook-Pro.local>
2018-11-22 17:13:09 +0700

    Added tos webview

--- src/screens/misc/PrivacyPolicy.tsx
+++ src/screens/misc/PrivacyPolicy.tsx
@@ -1,8 +1,10 @@
 import React from "react"
-import { ScrollView, StyleSheet } from "react-native"
+import { ScrollView, StyleSheet, WebView } from "react-native"
 
 import Text from "../../components/CustomText"
 import { NavigationStackScreenOptions } from "react-navigation"
+
+const TOS_HTML = require("../../../assets/tos.html")
 
 export default class PrivacyPolicy extends React.Component {
   static navigationOptions: NavigationStackScreenOptions = {
@@ -11,11 +13,12 @@ export default class PrivacyPolicy extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>
-        Mshwar Company ("we," "our" or "Mshwar") is committed to protecting the privacy of all visitors to our website Mshwarapp.com and all visitors who access our website or services through any mobile application (together, "Website"). Please read the following privacy policy which explains how we use and protect your information.
-        </Text>
-      </ScrollView>
+      // <ScrollView style={styles.container}>
+      //   <Text>
+      //   Mshwar Company ("we," "our" or "Mshwar") is committed to protecting the privacy of all visitors to our website Mshwarapp.com and all visitors who access our website or services through any mobile application (together, "Website"). Please read the following privacy policy which explains how we use and protect your information.
+      //   </Text>
+      // </ScrollView>
+      <WebView style={styles.container} source={TOS_HTML} scalesPageToFit={false} />
     )
   }
 }
--- src/screens/misc/Terms.tsx
+++ src/screens/misc/Terms.tsx
@@ -1,26 +1,24 @@
 import React from "react"
-import { ScrollView, StyleSheet,View,WebView } from "react-native"
+import { ScrollView, StyleSheet, WebView } from "react-native"
 
 import Text from "../../components/CustomText"
 import { NavigationStackScreenOptions } from "react-navigation"
 
+const TOS_HTML = require("../../../assets/tos.html")
+
 export default class Terms extends React.Component {
   static navigationOptions: NavigationStackScreenOptions = {
-    title: "Mshwar Terms of Service"
+    title: "Terms of Service"
   }
 
   render() {
-    var htmlCode = "<b>I am rendered in a <i>WebView</i></b>";
     return (
-      <ScrollView style={styles.container}>
-        <View style = {{
-    justifyContent: 'center',
-    alignItems: 'center'}} >
-          <Text style={{alignSelf:'center',justifyContent:'center',alignItems:'center'}}>
-          Welcome to Mshwarapp.com website and applications (each our "Service"). This page (together with the documents referred to on it) tells you the terms and conditions on which our partner restaurants supply any of their meals (the "Meals") listed on our site to you. Please read these terms and conditions carefully before ordering any Meals from our applications. By accessing our website and placing an order, you agreed to be bound by these terms and conditions and our terms of use policy.
-          </Text>
-        </View>
-      </ScrollView>
+      // <ScrollView style={styles.container}>
+      //   <Text>
+      //   Mshwar Company ("we," "our" or "Mshwar") is committed to protecting the privacy of all visitors to our website Mshwarapp.com and all visitors who access our website or services through any mobile application (together, "Website"). Please read the following privacy policy which explains how we use and protect your information.
+      //   </Text>
+      // </ScrollView>
+      <WebView style={styles.container} source={TOS_HTML} scalesPageToFit={false} />
     )
   }
 }
