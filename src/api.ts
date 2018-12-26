9f76536ee9bb01e8513e5afff91a25308585c1d4
deden1103 <de1103den@gmail.com>
2018-12-26 11:22:34 +0700

    add lang param

--- src/api.ts
+++ src/api.ts
@@ -4,10 +4,24 @@ class API {
   client: AxiosInstance
 
   constructor() {
-    this.client = axios.create({
+    let client = axios.create({
       baseURL: "https://admin.mshwarapp.com/api",
       headers: { "Content-Type": "application/json" }
     })
+
+    client.interceptors.request.use(function(config) {
+      if(config.method === 'post') {
+        config.data = {...config.data, lang: 'ar'}  
+        client.defaults.headers.common['Lang'] = 'ar'
+      }
+      console.log('config interceptor :', config)
+      return config
+    }, function(error) {
+      console.log('ERROR interceptor :', error)
+      return Promise.reject(error);
+    })
+
+    this.client = client
   }
 
   changeToken(token: string) {
