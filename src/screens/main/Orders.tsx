39908d11226517f4647c395ecb694a5af96ede40
deden1103 <de1103den@gmail.com>
2019-01-08 17:13:50 +0700

    tabBarComponent

--- src/screens/main/Orders.tsx
+++ src/screens/main/Orders.tsx
@@ -1,98 +1,21 @@
 import React from "react"
-import { View, StyleSheet, Image, FlatList } from "react-native"
-import moment from "moment"
 
-import Text from "../../components/CustomText"
-import { NavigationTabScreenOptions } from "react-navigation"
-import metrics from "../../config/metrics"
-import OrderItem from "../../components/OrderItem"
-import HeaderOverlay from "../../components/HeaderOverlay"
-import api from "../../utils/api"
-import strings from "../../components/language"
-import Lang from "../../components/Lang"
-import { keys } from '../../config/keys'
-import { getData } from '../../utils/storage'
+import { createTabNavigator } from "react-navigation"
+import OrdersTopTab from "../../components/OrdersTopTab"
 
-interface State {
-  isDataLoading: boolean
-  data: Array<any>
-}
+import OrdersHistory from "./OrdersHistory"
+import OrdersOnging from "./OrdersOnging"
 
-export default class Orders extends React.Component<any, State> {
-  state = {
-    isDataLoading: true,
-    data: []
-  }
-
-  async componentDidMount() {
-    this.setState({ isDataLoading: true })
-    const { data } = await api.client.get<any>("/orders")
-    console.log(data)
-    this.setState({ data: data, isDataLoading: false })
-  }
-  _onSetLanguage = async () => {
-    const languageStore = await getData(keys.language)
-    return await strings.setLanguage(languageStore)
-  }
-
-  componentWillMount = () => {
-    this._onSetLanguage()
-  }
-
-  render() {
-    console.log("data", this.state.data)
-    return (
-      <View style={styles.container}>
-        <HeaderOverlay />
-        <Lang styleLang={styles.title} language="ordersTitle" />
-        <Lang
-          styleLang={styles.subtitle}
-          language={this.state.data.length === 0 ? "ordersEmpty" : "ordersList"}
-        />
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item }: { item: any }) => {
-            return (
-              <OrderItem
-                name={item.name}
-                date={item.ordered_at}
-                statusText={item.status_text}
-              />
-            )
-          }}
-          style={styles.list}
-          refreshing={this.state.isDataLoading}
-          onRefresh={() => this.componentDidMount()}
-        />
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+export default createTabNavigator(
+  {
+    Onging: { screen: OrdersOnging },
+    History: { screen: OrdersHistory }
   },
-
-  list: {
-    paddingTop: 20
-  },
-
-  title: {
-    fontSize: 23,
-    color: "white",
-    marginTop: 50,
-    alignSelf: "flex-start",
-    marginLeft: 20
-  },
-  subtitle: {
-    fontSize: 18,
-    fontWeight: "300",
-    alignSelf: "flex-start",
-    marginLeft: 20,
-    color: "white",
-    marginTop: 20
+  {
+    tabBarComponent: ({ navigation }) => <OrdersTopTab navigation={navigation} />,
+    tabBarPosition: "top",
+    lazy: true,
+    swipeEnabled: true,
+    animationEnabled: true
   }
-})
+)
---
+++ src/screens/main/OrdersHistory.tsx
@@ -0,0 +1,99 @@
+import React from "react"
+import { View, StyleSheet, Image, FlatList } from "react-native"
+import moment from "moment"
+
+import Text from "../../components/CustomText"
+import { NavigationTabScreenOptions } from "react-navigation"
+import metrics from "../../config/metrics"
+import OrderItem from "../../components/OrderItem"
+import HeaderOverlay from "../../components/HeaderOverlay"
+import api from "../../utils/api"
+import strings from "../../components/language"
+import Lang from "../../components/Lang"
+import { keys } from '../../config/keys'
+import { getData } from '../../utils/storage'
+
+interface State {
+  isDataLoading: boolean
+  data: Array<any>
+}
+
+export default class Orders extends React.Component<any, State> {
+  state = {
+    isDataLoading: true,
+    data: []
+  }
+
+  async componentDidMount() {
+    this.setState({ isDataLoading: true })
+    const { data } = await api.client.get<any>("/orders")
+    console.log(data)
+    this.setState({ data: data, isDataLoading: false })
+  }
+  _onSetLanguage = async () => {
+    const languageStore = await getData(keys.language)
+    return await strings.setLanguage(languageStore)
+  }
+
+  componentWillMount = () => {
+    this._onSetLanguage()
+  }
+
+  render() {
+    console.log("data", this.state.data)
+    return (
+      <View style={styles.container}>
+        {this.state.data.length === 0 && (
+          <Lang
+            styleLang={styles.subtitle}
+            language="ordersEmpty"
+          />
+        )}
+        
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }: { item: any }) => {
+            return (
+              <OrderItem
+                name={item.name}
+                date={item.ordered_at}
+                statusText={item.status_text}
+              />
+            )
+          }}
+          style={styles.list}
+          refreshing={this.state.isDataLoading}
+          onRefresh={() => this.componentDidMount()}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  list: {
+    paddingTop: 20
+  },
+
+  title: {
+    fontSize: 23,
+    color: "white",
+    marginTop: 50,
+    alignSelf: "flex-start",
+    marginLeft: 20
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "300",
+    alignSelf: "flex-start",
+    marginLeft: 20,
+    color: "white",
+    marginTop: 20
+  }
+})
