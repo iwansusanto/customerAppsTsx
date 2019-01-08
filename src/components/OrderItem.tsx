9af3c36187ae51d5d22ff557161f416ceda4219a
deden1103 <de1103den@gmail.com>
2019-01-08 18:15:40 +0700

    add icon and condition when scheduled order

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -15,6 +15,7 @@ import Text from "../components/CustomText"
 const PICTURE = require("../../assets/dummy_order.png")
 const ICON_PHONE = require("../../assets/ic_phone_fill.png")
 const ICON_MESSAGE = require("../../assets/ic_message.png")
+const ICON_ARROW = require("../../assets/ic_arrow_blue.png")
 
 interface Props extends TouchableOpacityProps {
   name: string
@@ -26,18 +27,28 @@ export default (props: Props) => (
   <TouchableOpacity style={styles.container} {...props}>
     <Image source={PICTURE} style={styles.image as ImageStyle} />
     <View style={styles.detailContainer}>
-      <Text style={styles.title}>{props.name}</Text>
+      <View style={styles.titleCard}>
+        <Text style={styles.title}>{props.name}</Text>
+        {props.statusText === 'SCHEDULED' && (
+          <View style={{flex: 1}}>
+            <Image source={ICON_ARROW} style={styles.image as ImageStyle} />  
+          </View>
+        )}
+      </View>
       <Text style={styles.status}>{props.statusText}</Text>
       <Text style={styles.date}>{moment(props.date).format("DD MMM, hh:mm a")}</Text>
     </View>
-    <View style={styles.iconContainer}>
-      <TouchableOpacity>
-        <Image source={ICON_PHONE} />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Image source={ICON_MESSAGE} />
-      </TouchableOpacity>
-    </View>
+    {props.statusText !== 'SCHEDULED' && (
+      <View style={styles.iconContainer}>
+        <TouchableOpacity>
+          <Image source={ICON_PHONE} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={ICON_MESSAGE} />
+        </TouchableOpacity>
+      </View>
+    )}
+    
   </TouchableOpacity>
 )
 
@@ -57,7 +68,10 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     shadowOpacity: 1
   },
-
+  titleCard: {
+    flex: 1, 
+    flexDirection: 'row'
+  },
   detailContainer: {
     marginLeft: 10,
     justifyContent: "center",
@@ -66,7 +80,8 @@ const styles = StyleSheet.create({
 
   title: {
     fontWeight: "bold",
-    fontSize: 13
+    fontSize: 13,
+    flex: 9
   },
 
   status: {
